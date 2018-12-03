1444a8cbbc1a0b5a3098896dbe3e8c7e33ba4894
anirudhmalode <2016mns015@sggs.ac.in>
2018-12-02 23:41:33 -0800

    Updated & commented

--- js/app.js
+++ js/app.js
@@ -10,9 +10,11 @@ const client_id = "Iv1.f6caebe217ab3b44";
 const client_secret = "1a259f9cb1dba567baa6aba560d101ce8783d585";
 
 
-
+// For fetching the information of user using github API.
+// Async await call.
 const fetchUsers = async (user) => {
     const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+
 
     const data = await api_call.json();
     return { data }; // same as { data : data } written is ES6 feature.
@@ -24,6 +26,7 @@ const showData = () => {
     fetchUsers(inputValue.value).then((res) => {
         console.log(res);
 
+        // All the properties that can be applied for any github user.
         nameContainer.innerHTML = `Name: <span class="main__profile-value"> ${res.data.name} </span>`;
         unContainer.innerHTML = `Username: <span class="main__profile-value"> ${res.data.login} </span>`;
         reposContainer.innerHTML = `Repositories: <span class="main__profile-value"> ${res.data.public_repos} </span>`;
